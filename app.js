757b066c8ad083ee1faf9faf538ed08b95799415
vikaskushwaha97/CBT <vicky99vmk@gmail.com>
2026-07-08 02:38:23 +0530

    REST API using only Express and the built-in fs module.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
+app.use(express.json());
 
 // GET /files
 app.get("/files", (req, res) => {
@@ -30,9 +31,44 @@ app.get("/file/:filename", (req, res) => {
     });
 });
 
-// Any other route
-app.use((req, res) => {
-    res.status(404).send("Not Found");
+// POST /file
+app.post("/file", (req, res) => {
+    const { filename, content } = req.body;
+    const filePath = path.join(__dirname, "files", filename);
+
+    fs.writeFile(filePath, content, (err) => {
+        if (err) {
+            return res.status(500).json({
+                error: "Unable to write file"
+            });
+        }
+
+        res.status(201).json({ message: "File created successfully" });
+    });
+});
+
+app.put("/file/:filename", (req, res) => {
+    const filePath = path.join(__dirname, "files", req.params.filename);
+
+    fs.writeFile(filePath, req.body.content, (err) => {
+        if (err) {
+            return res.status(500).send("Unable to update file");
+        }
+
+        res.send("File updated successfully");
+    });
+});
+
+app.delete("/file/:filename", (req, res) => {
+    const filePath = path.join(__dirname, "files", req.params.filename);
+
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            return res.status(404).send("File not found");
+        }
+
+        res.send("File deleted successfully");
+    });
 });
 
 module.exports = app;

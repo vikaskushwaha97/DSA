81df491e7e4b80539754d90dd393634f87f88981
vikaskushwaha97/CBT <vicky99vmk@gmail.com>
2026-07-08 02:46:17 +0530

    map fuction to create a new array where each element is the average of its neighbors and itself.

--- app.js
+++ app.js
@@ -70,5 +70,27 @@ app.delete("/file/:filename", (req, res) => {
         res.send("File deleted successfully");
     });
 });
-
+const numbers = [3, -1, 1, 4, 1, 5, 9, 2, 6];
+const averaged = numbers
+  .filter((num) => num > 0)
+  .map((num, idx, arr) => {
+    // Without the arr argument, there's no way to easily access the
+    // intermediate array without saving it to a variable.
+    const prev = arr[idx - 1];
+    const next = arr[idx + 1];
+    let count = 1;
+    let total = num;
+    if (prev !== undefined) {
+      count++;
+      total += prev;
+    }
+    if (next !== undefined) {
+      count++;
+      total += next;
+    }
+    const average = total / count;
+    // Keep two decimal places
+    return Math.round(average * 100) / 100;
+  });
+console.log(averaged); // [2, 2.67, 2, 3.33, 5, 5.33, 5.67, 4]
 module.exports = app;

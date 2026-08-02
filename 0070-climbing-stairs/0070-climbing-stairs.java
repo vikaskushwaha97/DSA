c3a3c87815ac8e441cead80b76d054a4019c0f87
Vikas Kushwaha <vicky99vmk@gmail.com>
2026-08-03 00:39:58 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.58%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[]= new int [n+1];
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
+          }
+
+     private int helper (int n, int dp[]){
+
+        if(n<=2) {
+            return n;
+            }
+        if(dp[n] != -1 ){
+            return dp[n];
+            }
+        
+        else dp[n]=helper(n-1,dp)+helper(n-2,dp);
+        return dp[n];
+        
+        
+     
+    }
+}

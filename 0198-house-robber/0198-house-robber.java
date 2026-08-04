faa412d27d6b3337d17b089625f9698d706577b2
Vikas Kushwaha <vicky99vmk@gmail.com>
2026-08-05 02:22:39 +0530

    Time: 0 ms (100%), Space: 42.9 MB (19.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,21 @@
-//Memoization Solution practise
+//tabulation practise for robber question
 class Solution {
     public int rob(int[] nums) {
-    int []dp =new int [nums.length];
-    Arrays.fill(dp,-1);
-    return Helper(nums.length-1,dp,nums);
-        }
-    private int Helper(int n,int[]dp,int[]nums){
-        int pick;
-        int notpick;
-        if (n==0) return nums[0];
-        if (n<0) return 0;
-        if (dp[n]!=-1) return dp[n];
-        else {pick= Helper(n-2,dp,nums)+nums[n];
-              notpick=Helper(n-1,dp,nums);
-              dp[n]=Math.max(pick,notpick);}
-              return dp[n];
-         }
+        int n=nums.length;
+        int[]dp=new int [n];
+         
+          if(n==1) return nums[0];
+          if(n==2) return Math.max(nums[0],nums[1]);
+
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+
+
+         
+          for (int i =2; i<n;i++){
+
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+
+          }return dp[n-1];
+    }
 }
